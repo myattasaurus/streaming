46ab35353331745f25c4e936bdd9d08411cf27f9
myattasaurus <rcoins84@yahoo.com>
2024-11-20 16:34:36 -0500

    folder structure

--- index.js
+++
@@ -1,8 +0,0 @@
-function store() {
-    let text = document.getElementById('store').value;
-    localStorage.setItem('store', text);
-}
-
-function show() {
-    document.getElementById('show').innerHTML = localStorage.getItem('store');
-}
